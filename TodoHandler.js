804039cacd02a0e7256a2366fd7eef78b9136c50
ladanov <ladanov_n@mail.ru>
2019-01-12 18:29:39 +0300

    added: _getTodos method

--- TodoHandler.js
+++ TodoHandler.js
@@ -17,8 +17,32 @@ module.exports = class TodoHandler {
                         filename: getFileName(filePath),
                         data
                     }
-                }) 
+                });
         }));
+        this.uploadedFiles.forEach(data => {
+            this._getTodos(data.data);
+        });
     }
 
+    _getTodos(data) {
+        let result;
+        const commentReg = /\/\/\s*TODO[\s:]+(.+)/gmi;
+        const specMarkupReg = /([a-zа-я]+);\s*(\d{4}-\d{2}-\d{2});\s*(.+)/i;
+        while (result = commentReg.exec(data)) {
+            const todo = result[1];
+            const specMarkupTodo = todo.match(specMarkupReg);
+
+            if (specMarkupTodo) {
+                this.uploadedTODOs.push({
+                    author: specMarkupTodo[1],
+                    date: new Date(specMarkupTodo[2]),
+                    comment: specMarkupTodo[3]
+                });
+            } else {
+                this.uploadedTODOs.push({
+                    comment: todo
+                });
+            }
+        }
+    }
 }
