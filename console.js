5a771da57090e89160ed912b756d5b0cd39e8bf6
ladanov_n <ladanov_n@mail.ru>
2019-01-16 09:56:10 +0300

    fixed: stdin encoding

--- console.js
+++ console.js
@@ -1,10 +1,11 @@
 const readline = require('readline');
 const {stdin, stdout} = process;
 
+stdin.setEncoding('utf8');
 const rl = readline.createInterface(stdin, stdout);
 
 function readLine(callback) {
-    rl.on('line', callback); // TODO pe; 2015-08-10; а какая будет кодировка?
+    rl.on('line', callback);
 }
 
 function writeLine(date) {
